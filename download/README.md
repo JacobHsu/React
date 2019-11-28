bdafe6077ade049ff74c5508a558ffb71e2d426e
jacobhsu <jacob.hsu.tw@gmail.com>
2019-11-28 16:51:30 +0800

    download qrcode.react

--- download/README.md
+++ download/README.md
@@ -1,5 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+[![NPM](https://nodei.co/npm/qrcode.react.png?downloads=true&stars=true)](https://nodei.co/npm/qrcode.react/)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- download/src/App.js
+++ download/src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import QRCode from 'qrcode.react'
+
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <QRCode value="http://facebook.github.io/react/" id="canvas"/>
+        <br></br>
+        <button type="button" onClick={()=>{
+              var canvas = document.getElementById("canvas");
+              var dataURL = canvas.toDataURL();
+
+              var a = document.createElement('a');
+
+              var fileName = 'qrCode.png';
+              a.href = dataURL;
+              a.download = fileName;
+              a.click();
+              window.URL.revokeObjectURL(dataURL);
+          }}>
+           download
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +35,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
